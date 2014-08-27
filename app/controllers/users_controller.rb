15f0141db0436cc5541a45adbb2b4e672a06ee52
navneet <user@navneet.local>
2014-08-27 18:44:46 +0530

    finished crud

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+
+  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_filter :correct_user,   only: [:edit, :update]
+  before_filter :admin_user,     only: :destroy
+
   def new
     @user = User.new
   end
@@ -19,6 +24,57 @@ class UsersController < ApplicationController
   end
 
   def index
-    @users = User.all
+    #@users = User.all
+    @users = User.paginate(page: params[:page])
+    #, per_page: 2
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      # Handle a successful update.
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    if !current_user?(@user)
+      @user.destroy
+      flash[:success] = "User destroyed."
+    else
+      flash[:warning] = "Cannot Delete self."
+    end
+    redirect_to users_url
+  end
+
+
+  private
+
+    def signed_in_user
+      # redirect_to signin_url, notice: "Please sign in." unless signed_in?
+      unless signed_in?
+        store_location
+        flash[:notice] = "Please sign in."
+        redirect_to signin_url
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
+
 end
